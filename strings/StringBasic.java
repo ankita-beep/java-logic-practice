ff78a94b42d582ec06f374e3fce685e715042c2d
Ankita Haldar <174636962+ankita-beep@users.noreply.github.com>
2026-02-01 23:37:52 +0530

    Add basic string example

--- strings/StringBasic.java
+++ strings/StringBasic.java
@@ -1,3 +1,11 @@
 public class StringBasic {
-    
+   
+    public static void main(String[] args) {
+        String name = "Ankita";
+        System.out.println(name);
+        System.out.println(name.length());
+    }
 }
+
+    
+
